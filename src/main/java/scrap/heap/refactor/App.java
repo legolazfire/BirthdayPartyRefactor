8b95418b7bd7de9bbe7534d1d952b2976408152a
Karthik Maruthavanan <kmaruthavanan@ebay.com>
2019-10-05 10:16:52 -0700

    Remove dead code

--- src/main/java/scrap/heap/refactor/App.java
+++ src/main/java/scrap/heap/refactor/App.java
@@ -49,26 +49,4 @@ public class App {
         party.placeOrders();
 
     }
-
-    private static void order(String balloonColor, String material, String number, String flavor, String frostingFlavor, String shape, String size, String cakeColor) {
-
-        orderBalloons(balloonColor, material, number);
-
-        orderCake(frostingFlavor, flavor, shape, size, cakeColor);
-    }
-
-    private static void orderBalloons(String balloonColor, String material, String number) {
-
-        //for the purposes of this exercise, pretend this method works and adds balloons to the model
-        System.out.println("Balloons ordered; " + balloonColor + ", " + material + ", " + number);
-
-    }
-
-    private static void orderCake(String flavor, String frostingFlavor, String shape, String size, String cakeColor) {
-
-        //for the purposes of this exercise, pretend that this method adds a cake to the model
-        System.out.println("cake ordered; " + flavor + ", " + frostingFlavor + ", " + shape + ", " + size + ", " + cakeColor);
-
-    }
-
 }
